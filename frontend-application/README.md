27547e9534630fe6b42d59555abe625ae3b3cf93
manjulaPatil82 <manju8226@gmail.com>
2023-11-17 12:18:06 +0530

    Added user crud operation

---
+++ frontend-application/README.md
@@ -0,0 +1,5 @@
+## Angular 14 User Info sapians project: CRUD with Rest API
+
+Build an Angular 14 CRUD App example to consume Rest APIs, display, remove user info data.
+
+Run `ng serve`. The app will automatically reload if you change any of the source files.
---
+++ frontend-application/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { ErrorCatchingInterceptor } from './interceptors/error-catching.interceptor';
+import { AlphabetOnlyDirective } from './alphabet-only.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddUserComponent,
+    UsersListComponent,
+    AlphabetOnlyDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    {
+       provide: HTTP_INTERCEPTORS,
+       useClass: ErrorCatchingInterceptor,
+       multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
